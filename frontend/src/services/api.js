32c4a7f28a7362cbfac3d1ffa4ec011e142bd574
mgowthami2409 <135138717+mgowthami2409@users.noreply.github.com>
2025-10-10 11:02:51 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:9090/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "https://suprajit-portal.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
